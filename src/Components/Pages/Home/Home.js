0b60c430ed48a74bd33c44c8ea8623ccf8b2b02f
Karima-Aktari <karimaaktari911@gmail.com>
2022-06-26 16:50:45 +0600

    commit images

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -3,9 +3,9 @@ import Reviews from './Reviews/Reviews';
 
 const Home = () => {
     return (
-        <div>
-            <h2>Summer Art Camp! 5 Days of Artistis a Painting Monet Van Gogh, Matisse, & More</h2>
-            <p>Multi-Day Course @</p>
+        <div className="container mx-auto">
+            <h2 className="text-4xl font-bold">Summer Art Camp! 5 Days of Artistis a Painting Monet Van Gogh, Matisse, & More</h2>
+            <p className='text-gray-400 font-bold'>Multi-Day Course @</p>
             <div>
                 <Reviews />
             </div>
--- src/Components/Pages/Home/Rivew/Review.js
+++ src/Components/Pages/Home/Rivew/Review.js
@@ -1,26 +1,34 @@
 import React from 'react';
+import star from '../../../../Images/star.png';
+import heart from '../../../../Images/heart.png';
+import share from '../../../../Images/share.png';
 
 const Review = (props) => {
-    const { id, description, teacherImage, teacherName, img1, img2, img3 } = props.review;
+    const { description, theacherImage, teacherName, teacherReview, classReview, completed, img1, img2, img3 } = props.review;
     return (
-        <div className='mx-auto'>
-            <h4>{id}</h4>
-            <div className='grid sm:grid-col-2 md:grid-col-2 gap-2'>
-                <div>
-                    <h4>{description}</h4>
+        <div className='container mx-auto my-4'>
+
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-2'>
+                <div className='container'>
+                    <h2 className='font-bold text-gray-700'>{description}</h2>
+                    <div className='flex sm:flex-col md:flex-row p-4'>
+                        <img src={theacherImage} className="w-16 h-16 rounded-full" alt="" />
+                        <h3 className='px-2 flex items-center font-bold text-xl text-indigo-800'>{teacherName}</h3>
+                    </div>
                     <div>
-                        <img src={teacherImage} alt="" />
-                        <h3>{teacherName}</h3>
+                        <p><span></span> {teacherReview}</p>
+                        <p>{classReview}</p>
+                        <h3>{completed}</h3>
                     </div>
                 </div>
 
-                <div >
+                <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-2'>
                     <div>
-                        <img src={img1} className="w-" alt="" />
+                        <img src={img1} className="h-full" alt="" />
                     </div>
-                    <div className='flex sm:flex-col'>
-                        <img src={img2} className="" alt="" />
-                        <img src={img3} alt="" />
+                    <div className='flex flex-col'>
+                        <img src={img2} className="h-40" alt="" />
+                        <img src={img3} classsName="h-40" alt="" />
                     </div>
                 </div>
             </div>
